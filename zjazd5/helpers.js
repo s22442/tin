9eea077945181d84a7c872fe0c828c45d65bb974
brofrain <downloading130@gmail.com>
2021-12-07 08:39:37 +0100

    fix

--- zjazd5/helpers.js
+++ zjazd5/helpers.js
@@ -1,4 +1,4 @@
-// podobnie jak w przypadku zjazdu 4 chcemy, aby aktualizacje DOM były automatyczne
+// podobnie jak w przypadku zjazdu 4 chcemy, aby aktualizacje DOM w zadaniu 1 i 2 były automatyczne
 // poprawia to czytelność kodu i jego odporność na błędy
 // https://v3.vuejs.org/api/basic-reactivity.html#reactive
 // reactive() zwraca reaktywną kopię obiektu podanego jako parametr
